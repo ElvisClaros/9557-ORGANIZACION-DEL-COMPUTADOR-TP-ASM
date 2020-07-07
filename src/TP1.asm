98800b7ae526b1dea14a483a496a15ded6596ede
ElvisClaros <celvisc@gmail.com>
2020-07-06 23:33:06 -0300

    Trabajo Practico terminado

--- src/TP1.asm
+++ src/TP1.asm
@@ -10,28 +10,40 @@ section  .data
 	msgErrOpen	            db      "Error en apertura de archivo",10,0
 
     msgIngOp                db      "Ingrese un operando de longitud 16 (caracteres 0 o 1) de la forma:%s ej. 1111111110000110",10,0
-    saltoLinea              db      "",10,0
+    saltoLinea              db      '',10,0
 
-    posOp                   dd      0
+    posOp                   dq      0
+
+    printInicio             db      "Inicio de operaciones...",13,10,0
+    printResul              db      "%16s %3s %16s = %16s",10,0
+    stringOr                db      " or"
+    stringXor               db      "xor"
+    stringAnd               db      "and"
+
+;Esto '',0 es para printf
+    opString                db      "   "
+                            db      '',0
+    resultado    times  16  db      " "
+                            db      '',0
+    operandoUno  times  16  db      " "
+                            db      '',0
+    operandoDos  times  16  db      " "
+                            db      '',0
+    varOp        times  16  db      " "
 
 	registro	times   0 	db      ""
 	operando	times   16	db      " "
-	operador				db      ' '
-	finReg                  db      "",13,10,0
-    unString    times   17  db      0
-	formScanfString         db      "%16s",0 ;solo lee 16 caracteres
-    formPrintString         db      "Ingreso :---->%s",13,10,0
+	operador               	db      " "
 
-    formPrint               db      "Ingreso :---->%i",13,10,0
-    formScanf               db      "%i",0
-    unEntero    times   1   dd      0
+	formScanfString         db      "%16s",0 ;solo lee 16 caracteres
+    unString    times   30  db      "f",0
 section  .bss
     plusRsp		resq	1
 	fileHandle	resq	1
 	esValid		resb	1
+	estadoBit   resb	1
 	contador	resq	1
-	varOp       times   16  resb      1
-	buffer		resb	10
+	posBits     resq    1
 
 section .text
 
@@ -45,12 +57,17 @@ pedirOp:
     cmp		byte[esValid],'S'
     jne		pedirOp
 
+    mov     rcx,16
+    lea     rsi,[unString]
+    lea     rdi,[operandoUno]
+    rep     movsb
+
+    call    inicioOperaciones
+
 	call	abrirArch
 	cmp		qword[fileHandle],0				;Error en apertura?
 	jle		errorOpen
     call	leerArch
-    call    mostrarString
-    call    mostrarReg
 endProg:
 	ret
 
@@ -62,28 +79,6 @@ errorOpen:
     add		rsp,[plusRsp]
 	jmp		endProg
 ;------------------------------------FIN-MAIN---------------------------
-
-;************************************INICIO-RUTINA*********************
-mostrarMsgOp:
-	mov     rdi,msgIngOp
-	mov     rsi,saltoLinea
-    call	checkAlign
-    sub		rsp,[plusRsp]
-    call	printf
-    add		rsp,[plusRsp]
-	ret
-;*************************************FIN-RUTINA***********************
-
-;************************************INICIO-RUTINA*********************
-scanfEntero:
-    lea     rdi,[formScanf]
-    lea     rsi,[unEntero]
-    call	checkAlign
-    sub		rsp,[plusRsp]
-    call	scanf
-    add		rsp,[plusRsp]
-    ret
-;*************************************FIN-RUTINA***********************
 
 ;************************************INICIO-RUTINA*********************
 scanfString:
@@ -117,10 +112,15 @@ leerReg:
 	cmp		rax,0				   ;Fin de archivo?
 	jle		eof                    ;<=0
 
-;    call   VALREG
+    mov     rcx,16
+    lea     rsi,[operando]
+    lea     rdi,[operandoDos]
+    rep     movsb
+
+    call   VALREG
     cmp		byte[esValid],'S'
     jne		leerReg                 ;<>
-
+    call    OPERAR
     jmp     leerReg
 
 eof:
@@ -129,6 +129,230 @@ eof:
 	call	fclose
 	ret
 ;*************************************FIN-RUTINA***********************
+;************************************INICIO-RUTINA*********************
+OPERAR:
+
+    cmp     byte[operador],'O'
+    je      orString
+    cmp     byte[operador],'X'
+     je     xorString
+    cmp     byte[operador],'N'
+    je      andString
+    ret
+;*************************************FIN-RUTINA***********************
+
+
+;:::::::::::::::::::::::::::::::::::INICIO-AND:::::::::::::::::::::::::
+;************************************INICIO-RUTINA*********************
+andString:
+    mov     qword[posBits],0
+    mov     rcx,16
+compBitsAnd:
+    call    compararBitAnd
+    loop    compBitsAnd
+
+    mov     rcx,3
+    lea     rsi,[stringAnd]
+    lea     rdi,[opString]
+    rep     movsb
+    call    printResultado
+
+    mov     rcx,16
+    lea     rsi,[resultado]
+    lea     rdi,[operandoUno]
+    rep     movsb
+    ret
+;*************************************FIN-RUTINA***********************
+;************************************INICIO-RUTINA*********************
+compararBitAnd:
+    mov     qword[contador],rcx
+    mov		rcx,0
+    mov     rax,0
+
+    mov     rbx,qword[posBits]
+    mov		al,byte[operandoUno + rbx]
+    mov     cl,byte[operandoDos + rbx]
+
+    cmp     rcx,rax
+    je      bitIgualesAnd
+
+    mov     byte[resultado + rbx],'0'
+    mov		rcx,qword[contador]
+    add     rbx,1
+    mov     qword[posBits],rbx
+    ret
+bitIgualesAnd:
+    cmp     byte[operandoDos + rbx],'1'
+    je      sonUnoAnd
+    mov     byte[resultado + rbx],'0'
+    mov		rcx,qword[contador]
+    add     rbx,1
+    mov     qword[posBits],rbx
+    ret
+sonUnoAnd:
+    mov     byte[resultado + rbx],'1'
+    mov		rcx,qword[contador]
+    add     rbx,1
+    mov     qword[posBits],rbx
+    ret
+;*************************************FIN-RUTINA***********************
+;::::::::::::::::::::::::::::::::::::::FIN-AND:::::::::::::::::::::::::
+
+
+
+;:::::::::::::::::::::::::::::::::::INICIO-XOR:::::::::::::::::::::::::
+;************************************INICIO-RUTINA*********************
+xorString:
+    mov     qword[posBits],0
+    mov     rcx,16
+compBitsXor:
+    call    compararBitXor
+    loop    compBitsXor
+
+    mov     rcx,3
+    lea     rsi,[stringXor]
+    lea     rdi,[opString]
+    rep     movsb
+
+    call    printResultado
+
+    mov     rcx,16
+    lea     rsi,[resultado]
+    lea     rdi,[operandoUno]
+    rep     movsb
+    ret
+;*************************************FIN-RUTINA***********************
+;************************************INICIO-RUTINA*********************
+compararBitXor:
+    mov     qword[contador],rcx
+    mov		rcx,0
+    mov     rax,0
+
+    mov     rbx,qword[posBits]
+    mov		al,byte[operandoUno + rbx]
+    mov     cl,byte[operandoDos + rbx]
+
+    cmp     rcx,rax
+    je      bitIgualesXor
+
+    mov     byte[resultado + rbx],'1'
+    mov		rcx,qword[contador]
+    add     rbx,1
+    mov     qword[posBits],rbx
+    ret
+bitIgualesXor:
+    mov     byte[resultado + rbx],'0'
+    mov		rcx,qword[contador]
+    add     rbx,1
+    mov     qword[posBits],rbx
+    ret
+;*************************************FIN-RUTINA***********************
+;:::::::::::::::::::::::::::::::::::::FIN-XOR::::::::::::::::::::::::::
+
+
+
+;:::::::::::::::::::::::::::::::::::INICIO-OR::::::::::::::::::::::::::
+;************************************INICIO-RUTINA*********************
+orString:
+    mov     qword[posBits],0
+    mov     rcx,16
+compBitsOr:
+    call    compararBitOr
+    loop    compBitsOr
+
+    mov     rcx,3
+    lea     rsi,[stringOr]
+    lea     rdi,[opString]
+    rep     movsb
+    call    printResultado
+
+    mov     rcx,16
+    lea     rsi,[resultado]
+    lea     rdi,[operandoUno]
+    rep     movsb
+    ret
+;*************************************FIN-RUTINA***********************
+;************************************INICIO-RUTINA*********************
+compararBitOr:
+    mov     qword[contador],rcx
+    mov		rcx,0
+    mov     rax,0
+
+    mov     rbx,qword[posBits]
+    mov		al,byte[operandoUno + rbx]
+    mov     cl,byte[operandoDos + rbx]
+
+    cmp     rcx,rax
+    je      bitIgualesOr
+
+    mov     byte[resultado + rbx],'1'
+    mov		rcx,qword[contador]
+    add     rbx,1
+    mov     qword[posBits],rbx
+    ret
+bitIgualesOr:
+    cmp     byte[operandoDos + rbx],'1'
+    je      sonUno
+    mov     byte[resultado + rbx],'0'
+    mov		rcx,qword[contador]
+    add     rbx,1
+    mov     qword[posBits],rbx
+    ret
+sonUno:
+    mov     byte[resultado + rbx],'1'
+    mov		rcx,qword[contador]
+    add     rbx,1
+    mov     qword[posBits],rbx
+    ret
+;*************************************FIN-RUTINA***********************
+;::::::::::::::::::::::::::::::::::::::FIN-OR::::::::::::::::::::::::::
+
+
+
+
+
+;$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$MENSAGES$$$$$$$$$$$$$$$$$$$$$$$$$$
+;************************************INICIO-RUTINA*********************
+mostrarMsgOp:
+	mov     rdi,msgIngOp
+	mov     rsi,saltoLinea
+    call	checkAlign
+    sub		rsp,[plusRsp]
+    call	printf
+    add		rsp,[plusRsp]
+	ret
+;*************************************FIN-RUTINA***********************
+
+;************************************INICIO-RUTINA*********************
+printResultado:
+    mov     rdi,printResul
+	mov		rsi,operandoUno
+	mov     rdx,opString
+	mov     rcx,operandoDos
+	mov     r8,resultado
+    call	checkAlign
+    sub		rsp,[plusRsp]
+    call	printf
+    add		rsp,[plusRsp]
+    ret
+;*************************************FIN-RUTINA***********************
+
+;************************************INICIO-RUTINA*********************
+inicioOperaciones:
+	mov     rdi,printInicio
+    call	checkAlign
+    sub		rsp,[plusRsp]
+    call	printf
+    add		rsp,[plusRsp]
+	ret
+;*************************************FIN-RUTINA***********************
+;$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$FIN-MENSAGES$$$$$$$$$$$$$$$$$$$$$$
+
+
+
+
+;#####################################-VALIDACION-######################
+
 ;************************************INICIO-RUTINA*********************
 validarScanf:
     mov     rcx,16
@@ -141,9 +365,16 @@ validarScanf:
 
 ;************************************INICIO-RUTINA*********************
 VALREG:
-
+    mov     rcx,16
+    lea     rsi,[operandoDos]
+    lea     rdi,[varOp]
+    rep movsb
     call    validarBits
     cmp		byte[esValid],'S'
+    je      valOperador
+    mov		byte[esValid],'N'
+    ret
+valOperador:
     mov		byte[esValid],'N'
     call    validarOperador
     ret
@@ -169,7 +400,6 @@ opInvalido:
     ret
 ;*************************************FIN-RUTINA***********************
 
-
 ;************************************INICIO-RUTINA*********************
 validarBits:
 
@@ -189,44 +419,15 @@ bitValido:
     cmp     rax,16                       ;valido los 16 bits
     jl      validarBits
     mov		byte[esValid],'S'
+    mov		qword[posOp],0
     ret
 bitInvalido:
     mov		qword[posOp],0
     mov		byte[esValid],'N'
     ret
 ;*************************************FIN-RUTINA***********************
+;#################################-VALIDACION-FIN-######################
 
-;************************************INICIO-RUTINA*********************
-mostrarReg:
-	mov		rdi,operando			;Parametro 2: campo que se encuentra en el formato indicado q se imprime por pantalla
-    call	checkAlign
-    sub		rsp,[plusRsp]
-    call	printf
-    add		rsp,[plusRsp]
-    ret
-;*************************************FIN-RUTINA***********************
-
-;************************************INICIO-RUTINA*********************
-mostrarEntero:
-	mov     rdi,formPrint
-	mov     rsi,[unEntero]
-    call	checkAlign
-    sub		rsp,[plusRsp]
-    call	printf
-    add		rsp,[plusRsp]
-	ret
-;*************************************FIN-RUTINA***********************
-
-;************************************INICIO-RUTINA*********************
-mostrarString:
-	mov     rdi,formPrintString
-	mov     rsi,unString
-    call	checkAlign
-    sub		rsp,[plusRsp]
-    call	printf
-    add		rsp,[plusRsp]
-	ret
-;*************************************FIN-RUTINA***********************
 
 	;----------------------------------------
     ;----------------------------------------
